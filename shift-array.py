0c73651b5c79ba7e37b355a19e655537be8adc06
Jay Adams <j.adams011@hotmail.com>
2018-03-20 23:06:14 -0700

    fleshed the code out more.  still doesnt quite work.

--- shift-array.py
+++ shift-array.py
@@ -1,10 +1,16 @@
 def insertShiftArray(array, value):
-    middle = len(array)//2
-    new_array = [0]*(len(array)+1)
-    for item in range(len(new_array)):
-        if i < (len(array)):
-            new_array+1[i] = array[i]
-            elif i = middle:
-                else
-                new_array[i] = array[i+1]
-    return new_array+1
+    if len(array) > 0 and len(array) % 2 == 0:
+        middle = len(array)//2
+    elif len(array) > 0 and len(array) % 2 == 1:
+        middle = len(array)//2 + 1
+    else:
+        middle = 0
+        new_array = [0]*(len(array)+1)
+    for i in range(middle):
+        new_array[i] = array[i]
+        new_array[middle] = value
+    for i in range(middle + 1, len(new_array)):
+        new_array[i] = array[i-1]
+    print(new_array)
+
+    insertShiftArray([10, 20, 40, 50], 35)
