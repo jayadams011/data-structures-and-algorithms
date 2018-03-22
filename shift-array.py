0cc1b4562ee4e866222dddee95f0ebb4b5e3fc6a
Jay Adams <j.adams011@hotmail.com>
2018-03-21 19:41:44 -0700

    fleshed out tree.  fixed code in shift-array.py so it works.  Added test_shift-array.py file.

--- shift-array.py
+++ shift-array.py
@@ -1,16 +1,17 @@
-def insertShiftArray(array, value):
-    if len(array) > 0 and len(array) % 2 == 0:
-        middle = len(array)//2
-    elif len(array) > 0 and len(array) % 2 == 1:
-        middle = len(array)//2 + 1
+def insertShiftArray(old_array, value):
+    if len(old_array) > 0 and len(old_array) % 2 == 0:
+        middle = len(old_array)//2
+    elif len(old_array) > 0 and len(old_array) % 2 == 1:
+        middle = len(old_array)//2 + 1
     else:
         middle = 0
-        new_array = [0]*(len(array)+1)
+    new_array = [0]*(len(old_array)+1)
     for i in range(middle):
-        new_array[i] = array[i]
-        new_array[middle] = value
+        new_array[i] = old_array[i]
+    new_array[middle] = value
     for i in range(middle + 1, len(new_array)):
-        new_array[i] = array[i-1]
+        new_array[i] = old_array[i-1]
     print(new_array)
 
-    insertShiftArray([10, 20, 40, 50], 35)
+
+insertShiftArray([10, 20, 40, 50], 35)
