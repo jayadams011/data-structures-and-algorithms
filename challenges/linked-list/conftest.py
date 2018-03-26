3f1b4d17e83f79efaceefb8b871f1e3f3239a743
Jay Adams <j.adams011@hotmail.com>
2018-03-25 21:21:11 -0700

    added more code

---
+++ challenges/linked-list/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from node import Node
+from linked_list import LinkedList as LL
+
+@pytest.fixture
+def n_node():
+    return Node(1,None)
+
+@pytest.fixture
+def a_node():
+    return Node('a',None)
+
+@pytest.fixture
+def empty_linked_list():
+    return LL()
+
+@pytest.fixture
+def n_linked_list_list():
+    return LL([1,2,3])
+
+@pytest.fixture
+def m_linked_list_list():
+    return LL([1,'b',2,'d'])
+
+@pytest.fixture
+def linked_list_dict():
+    return LL({'a':1,'b':2,'c':3})
+
+@pytest.fixture
+def linked_list_tuple():
+    return LL((1,2,3))
