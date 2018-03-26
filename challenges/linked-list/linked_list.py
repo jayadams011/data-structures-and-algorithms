df457a91e8a4b66368767c3d4d613ecb868f10a5
Jay Adams <j.adams011@hotmail.com>
2018-03-25 18:59:13 -0700

    add code to flesh it out

---
+++ challenges/linked-list/linked_list.py
@@ -0,0 +1,28 @@
+from node import Node
+
+
+class SinglyLinkedList:
+    """This will be a link list class """
+    def __init__(self, iter=[]):
+        self._current = None
+        self.head = None
+        self._size = 0
+
+# l = LinkedList([1, 2, 3, 4])
+
+
+for item in reversed(iter):
+    self.insert(item)
+# l.head => 1 -> 2 -> 3 -> 4 ->
+
+
+def __repr__(self):
+    return
+
+
+def __len__(self):
+    return self.size
+
+
+def insert(self, val):
+    pass
