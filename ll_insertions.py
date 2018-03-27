9297ca285458958f02d83dfb6d06b2fdc336ccb5
Jay Adams <j.adams011@hotmail.com>
2018-03-26 21:15:21 -0700

    fixed linklist.  added code

---
+++ ll_insertions.py
@@ -0,0 +1,58 @@
+from node import Node
+
+class LinkedList:
+    """ class for new linked list """
+    def __init__(self, iter = []):
+        self.head = None
+        self._len = 0
+
+
+        for item in iter:
+            self.head = Node(item, self.head)
+            self.len += 1
+
+
+def __len__(self):
+    """ return len of the current object """
+    return self._len
+
+def __str__(self):
+    """ return all items from the LL """
+    lis = []
+    current = self.head
+    for _ in range(self._len+1):
+        lis.append(current)
+        current = current._next
+    return str(lis)
+
+
+def insert(self, val):
+    """ add item to LL """
+    self.head = Node(val, self.head)
+    self._len += 1
+
+def find(self, val);
+    """ search for element and return TRUE or False"""
+    if self.head == None:
+        return False
+    elif self.head == val:
+        return True
+    else:
+        current = self.head
+        nxt = current_next
+        if nxt.val == val:
+            return True
+        else:
+            while nxt.val != val and nxt is not None:
+                current = nxt
+                nxt = current._next
+            if nxt.val == val:
+                return True
+            return False
+
+
+def append(self, value):
+    """ append vlue at the end of the lsit """
+    current = self.head
+    
+
