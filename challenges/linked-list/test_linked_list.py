95e811a54443f74b32ee2a8ee0192be1df2b284b
Jay Adams <j.adams011@hotmail.com>
2018-03-29 14:50:58 -0700

    added todays challenge.  Fixed README

--- challenges/linked-list/test_linked_list.py
+++ challenges/linked-list/test_linked_list.py
@@ -1,15 +1,6 @@
 
 from linked_list import mergeLists
 
-# @pytest.fixture
-# def empty_linked_list():
-#     return LL()
-
-
-# @pytest.fixture
-# def n_linked_list_list():
-#     return LL([1, 2, 3])
-
 
 def test_mergeLists_xempty(empty_ll, n_linked_ll):
     mergeLists(empty_ll, n_linked_ll)
@@ -25,12 +16,14 @@ def test_mergeLists_dif_len(linked_list_zip, n_linked_ll):
     mergeLists(linked_list_zip, n_linked_ll)
     assert str(linked_list_zip) == str([9, 1, 8, 2, 7, 3, 6])
 
-#     @pytest.fixture
-# def linked_list_zip():
-#     return LL((9, 8, 7, 6))
 
-# def test_mergeLists_empty():
-#     return LL(xlist(1, 2, 3), ylist())
+def test_ll_loop(has_loop_ll):
+    assert has_loop_ll.hasLoop() is True
 
-# def test_mergeLists_len():
-#     return LL(xlist.len != ylist.len)
+
+def test_ll_loop_f(n_linked_ll):
+    assert n_linked_ll.hasLoop() is False
+
+
+def test_ll_loop_e(empty_ll):
+    assert empty_ll.hasLoop() is False
