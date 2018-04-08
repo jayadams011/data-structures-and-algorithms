26596f719e29627f6477bf807e3530e94304a2c4
Jay Adams <j.adams011@hotmail.com>
2018-04-08 15:00:53 -0700

    added pre_order, post_order and in_order.  Also added insert.

--- data_structures/binary_search_tree/bst.py
+++ data_structures/binary_search_tree/bst.py
@@ -1,36 +1,101 @@
-from .node import Node
+class Node:
+    """ Node class for bst"""
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return '<Node Val: {}'.format(self.val)
+
+    def __str__(self):
+        return str(self.val)
 
 
-class Bst:
+class BST:
     def __init__(self, iter=[]):
         self.root = None
         if iter:
             for item in iter:
                 self.insert(item)
 
-    def insert(root, val):
-        if val >= current.val:
-            current.right is None
-            current.right = Node(val)
         else:
-            current = current.right
-            _insert(current, val)
-        if val < self.root:
-            if self.root.left is None:
-                self.root.left = Node(val)
-            else:
-                current = current.left
-                _insert(current.val)
+            raise TypeError('must be iterable type')
 
+    def __repr__(self):
+        return '<BST Root {}>'.format(self.root.val)
 
-def in_order():
-    pass
+    def __str__(self):
+        return self.root.val
 
+    def pre_order():
+        def _walk(node=None):
+                if node is None:
+                    return False
+                current = self.root
+                # if current has a left child or a right child, traverse through those childern
+                return current.val
+                if current.left:
+                    _walk(current.left)
+                    return current.left
+                if current.right:
+                    _walk(current.right)
+                    return current.right
+                return current.val
 
-def pre_order():
-    pass
+        _walk(self.root)
 
+    def in_order(self, operation):
+            def _walk(node=None):
+                if node is None:
+                    return False
+                current = self.root
+                # if current has a left chile or a right child, traverse through those childern
+                if current.left:
+                    _walk(current.left)
+                return current.val
+                if current.right:
+                    _walk(current.right)
+                return current.val
 
-def post_order():
-    pass
+            _walk(self.root)
 
+    def post_order():
+        def _walk(node=None):
+            if node is None:
+                return False
+            current = self.root
+            # if current has a left child or a right child, traverse through those childern
+            if current.left:
+                _walk(current.left)
+                return current.left
+            if current.right:
+                _walk(current.right)
+                return current.right
+            return current.val
+            _walk(self.root)
+
+    def insert(self, val):
+            node = Node(val)
+            current = self.root
+
+            if self.root is None:
+                self.root = node
+                return node
+
+            while current:
+                if val >= current.val:
+                    if current.right is not None:
+                        current = current.right
+                    else:
+                        current.right = node
+                        break
+
+                elif val < current.val:
+                    if current.left is not None:
+                        current = current.left
+                    else:
+                        current.left = node
+                        break
+
+            return node
