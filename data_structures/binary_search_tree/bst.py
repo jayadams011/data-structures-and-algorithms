99f259e71fdcbbbf7998b6271993c90b4efe23f7
Jay Adams <j.adams011@hotmail.com>
2018-04-08 22:22:42 -0700

    fixed pre in and post.  added tests.

--- data_structures/binary_search_tree/bst.py
+++ data_structures/binary_search_tree/bst.py
@@ -1,3 +1,4 @@
+
 class Node:
     """ Node class for bst, this allows BST to create an empty Node whne there
      is not a node. """
@@ -12,11 +13,10 @@ class Node:
 
     def __str__(self):
         """ these return a string representtaion of Node value"""
-        return str(self.val)
 
 
 class BST:
-    """ BST will traverse the tree suing functions to return the val of
+    """ BST will traverse the tree using functions to return the val of
     nodes at respective palces.  """
     def __init__(self, iter=[]):
         self.root = None
@@ -25,69 +25,71 @@ class BST:
                 self.insert(item)
 
         else:
-            raise TypeError('must be iterable type')
+            return None
 
     def __repr__(self):
-        return '<BST Root {}>'.format(self.root.val)
-
-    def __str__(self):
-        return self.root.val
+        return '<Node Val: {}'.format(self.val)
 
     def pre_order(self, operation):
+        # import pdb; pdb.set_trace()
+        """ the preorder operation.  travereses
+        the tree retruning root, left right """
+
         def _walk(node=None):
-            """ if node is none - reurn false and make a new node and set
-             that as root """
+
             if node is None:
                 return False
-            current = self.root
-            """ if current has a left child or a right child, traverse
-                through those childern """
-            return current.val
-        """ returns the current value of the node before anything happens. """
-        if current.left:
-            _walk(current.left)
-            return current.left
-        if current.right:
-            _walk(current.right)
-            return current.right
-        operation(current.val)
+
+            current = node
+            operation(current.val)
+            if current.left:
+                _walk(current.left)
+
+            if current.right:
+                _walk(current.right)
 
         _walk(self.root)
 
     def in_order(self, operation):
+        # import pdb; pdb.set_trace()
+        """ traveres and returns values in order"""
         def _walk(node=None):
             if node is None:
                 return False
-            current = self.root
-            """ if current has a left chile or a right child, traverse
-            #  through those childern and returns the current value as it get 
-            # to the last left or right node"""
+            current = node
+            """ if current has a left child or a right child, traverse
+            #  through those childern and returns the current value as it get
+            # to the last left or right node """
+
             if current.left:
                 _walk(current.left)
+            operation(current.val)
             if current.right:
                 _walk(current.right)
-            operation(current.val)
 
         _walk(self.root)
 
     def post_order(self, operation):
+        # import pdb; pdb.set_trace()
+        """ traveres and returns values left, right and root """
         def _walk(node=None):
             if node is None:
                 return False
-            current = self.root
+            current = node
             """ if current has a left child or a right child,
             # traverse through those childern """
             if current.left:
                 _walk(current.left)
-                return current.left
+
             if current.right:
                 _walk(current.right)
-                return current.right
+
             operation(current.val)
-            _walk(self.root)
+        _walk(self.root)
 
     def insert(self, val):
-        """ inserts a value in the correct placed based on val and the tree"""
+        """ inserts a value in the correct place based on val and the
+        tree paramerters """
         node = Node(val)
         current = self.root
 
--- data_structures/binary_search_tree/test_bst.py
+++ data_structures/binary_search_tree/test_bst.py
@@ -1,2 +1,52 @@
-from .bst import bst
-import pytest
+def test_empty_bst_length(new_bst):
+    """ test that root is non with empty tree """
+    assert new_bst.root is None
+
+
+def test_empty_bst_insert(new_bst):
+    """ test that not empty after insert """
+    new_bst.insert(1)
+    assert new_bst.root is not None
+
+
+def test_data_bst_insert_negative_left(filled_bst):
+    """ test if a neg val can be inserted """
+    filled_bst.insert(-1)
+    assert filled_bst.root.left.left.left.left.val == -1
+
+
+def test_data_bst_in_order_traverse(filled_bst):
+    """ test for inorder taverse """
+    lst = []
+    filled_bst.in_order(lst.append)
+    assert lst == [1, 2, 3, 4, 6, 8, 9, 12]
+
+
+def test_data_bst_pre_order_traverse(filled_bst):
+    """ test for pre order traverse """
+    lst = []
+    filled_bst.pre_order(lst.append)
+    assert lst == [4, 3, 2, 1, 8, 6, 12, 9]
+
+
+def test_data_bst_post_order_traverse(filled_bst):
+    """ test for post order traveres """
+    lst = []
+    filled_bst.post_order(lst.append)
+    assert lst == [1, 2, 3, 6, 9, 12, 8, 4]
+
+
+def test_empty_bst_post_order(new_bst):
+    """ test if post oreder catches insert """
+    new_bst.insert(1)
+    assert new_bst.post_order is not None
+
+
+# def test_repr(filled_bst):
+""" testing for repr return """
+#     assert filled_bst.__repr__() == '<Node Val: {}'.format(self.val)
+
+
+# def test_str(filled_bst):
+""" Testing for str return """
+#     assert filled_bst.__str__() == [4, 3, 2, 1, 8, 6, 12, 9]
