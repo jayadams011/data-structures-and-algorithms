ef41d5eb69ed85a17dfecca623e108d2d15b967b
Jay Adams <j.adams011@hotmail.com>
2018-04-09 21:35:14 -0700

    fixed code

--- challenges/fizz_buzz_tree/fizzbuzztree.py
+++ challenges/fizz_buzz_tree/fizzbuzztree.py
@@ -1,30 +1,53 @@
 from .bst import BST
 
 
-def fizzbuzz(BST):
-    # import pdb; pdb.set_trace()
-    """ traveres and returns values in order"""
+# def fizzbuzz(BST):
+#     # import pdb; pdb.set_trace()
+#     """ traveres and returns values in order"""
+    # def _walk(node=None):
+    #     # import pdb; pdb.set_trace()
+    #     if node is None:
+    #         return False
+    #     current = node
+    #     """ if current has a left child or a right child, traverse
+    #     #  through those childern and returns the current value as it get
+    #     # to the last left or right node """
+
+    #     if current.left:
+    #         _walk(current.left)
+    #     x = current.val
+    #     if x % 3 == 0 and x % 5 == 0:
+    #         current.val == ('fizzbuzz')
+    #     elif x % 5 == 0:
+    #         current.val == ('buzz')
+    #     elif x % 3 == 0:
+    #         current.val == ('fizz')
+    #     if current.right:
+    #         _walk(current.right)
+
+    # _walk(BST.root)
+
+    # return BST
+
+"""function takes a binary tree as an argument """
+
+
+def fizzbuzztree(tree):
     def _walk(node=None):
-        # import pdb; pdb.set_trace()
         if node is None:
-            return False
-        current = node
-        """ if current has a left child or a right child, traverse
-        #  through those childern and returns the current value as it get
-        # to the last left or right node """
-
-        if current.left:
-            _walk(current.left)
-        x = current.val
-        if x % 3 == 0 and x % 5 == 0:
-            current.val == ('fizzbuzz')
-        elif x % 5 == 0:
-            current.val == ('buzz')
-        elif x % 3 == 0:
-            current.val == ('fizz')
-        if current.right:
-            _walk(current.right)
-
-    _walk(BST.root)
-
-    return BST
+            return
+        if node.left is not None:
+            _walk(node.left)
+        if node.val % 15 == 0:
+            node.val = "fizzbuzz"
+        elif node.val % 3 == 0:
+            node.val = "fizz"
+        elif node.val % 5 == 0:
+            node.val = "buzz"
+        if node.right is not None:
+            _walk(node.right)
+    if tree.root is None:
+        return False
+    else:
+        _walk(tree.root)
+    return tree
--- challenges/fizz_buzz_tree/test_fizzbuzz.py
+++ challenges/fizz_buzz_tree/test_fizzbuzz.py
@@ -1,5 +1,5 @@
 from .bst import BST
-from .fizzbuzztree import fizzbuzz as fb
+from .fizzbuzztree import fizzbuzztree as fb
 import pytest
 
 
@@ -13,22 +13,20 @@ def filled_bst():
     return BST([4, 3, 2, 1, 8, 6, 12, 9])
 
 
-def test_empty_bst_fizzbuzztree_in_order_traverse(new_bst):
-    new_bst = fb(new_bst)
-    lst = []
-    new_bst.in_order(lst.append)
-    assert lst == []
+def test_post_order_(new_bst):
+    """empty bst"""
+    assert fb(new_bst) is False
 
 
 def test_data_bst_fizzbuzztree_in_order_traverse(filled_bst):
     filled_bst = fb(filled_bst)
     lst = []
     filled_bst.in_order(lst.append)
-    assert lst == ['1', '2', 'fizz', '4', 'fizz', '8', 'fizz', 'fizz']
+    assert lst == [1, 2, 'fizz', 4, 'fizz', 8,  'fizz', 'fizz']
 
 
 def test_data_bst_fizzbuzztree_pre_order_traverse(filled_bst):
     filled_bst = fb(filled_bst)
     lst = []
     filled_bst.pre_order(lst.append)
-    assert lst == ['4', 'fizz', '2', '1', '8', 'fizz', 'fizz', 'fizz']
+    assert lst == [4, 'fizz', 2, 1, 8, 'fizz', 'fizz', 'fizz']
