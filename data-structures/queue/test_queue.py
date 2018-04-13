437876a3b8542e2776e5b035f6878d27d8d16250
Jay Adams <j.adams011@hotmail.com>
2018-04-13 07:36:30 -0700

    added more tests

--- data-structures/queue/test_queue.py
+++ data-structures/queue/test_queue.py
@@ -26,3 +26,36 @@ def test_emp_enqueue(empty_qu):
     empty_qu.enqueue(5)
     assert empty_qu.front.val == 10
     assert empty_qu.back.val == 5
+
+
+def test_empty_empty_queue(empty_qu):
+    """test empty queue"""
+    assert empty_qu.front is None
+    assert empty_qu.back is None
+    assert empty_qu._len == 0
+
+
+def test_empty_empty_add(empty_qu):
+    """test enqueue with empty"""
+    empty_q.enqueue(2)
+    assert empty_qu.front.val == 2
+    assert empty_qu._len == 1
+    assert empty_qu.back.val == 2
+
+
+def test_small_len(small_qu, empty_qu):
+    """test len method """
+    assert small_qu._len == 4
+    assert empty_qu._len == 0
+
+
+def test_small_str(small_qu):
+    """test str repr"""
+    assert str(small_qu) == '1 2 4 5'
+
+
+def test_small_enquue(small_qu):
+    """test front and back"""
+    small_q.enqueue(3)
+    assert small_qu.front.val == 1
+    assert small_qu.back.val == 3
--- data-structures/stack/test_stack.py
+++ data-structures/stack/test_stack.py
@@ -1,4 +1,4 @@
-# from .stack import Stack
+from .stack import Stack
 import pytest
 
 
@@ -13,6 +13,12 @@ def test_push_add_val(empty_stack):
     assert empty_stack.top.val == 5
 
 
+def test_small_len(small_st, empty_st):
+    """test len prebuid list"""
+    assert len(small_st) == 4
+    assert len(empty_st) == 0
+
+
 def test_push_add_multi_val(empty_stack):
     """ test push 3 times """
     empty_stack.push(5)
@@ -22,13 +28,24 @@ def test_push_add_multi_val(empty_stack):
     assert empty_stack.top._next.val == 6
 
 
+def test_small_push(small_st):
+    """test push method"""
+    small_st.push(2)
+    small_st.push(4)
+    assert len(small_st)
+
+
 def test_pop_take_val(empty_stack):
     """ raise error if stack is empty before pop """
     with pytest.raises(IndexError):
         empty_stack.pop()
 
 
-
+def test_small_pop(small_st):
+    """test pop method"""
+    current = small_st.top
+    assert small_st.pop() is current
+    assert len(small_st) == 3
 
 
 def test_peek_val_empty(small_stack, empty_stack):
