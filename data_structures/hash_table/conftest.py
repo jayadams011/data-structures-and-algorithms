1ae43ecbef1c57f8ce6cacf72df49327740fe0df
Jay Adams <j.adams011@hotmail.com>
2018-05-06 13:44:00 -0700

    added tests cleaned up code.

--- data_structures/hash_table/conftest.py
+++ data_structures/hash_table/conftest.py
@@ -14,3 +14,28 @@ def empty_hash_table():
 def basic_hash_table():
     """Not empty hash table."""
     return HashTable(1)
+
+
+@pytest.fixture
+def small_table():
+    """Small table."""
+    small_table = HashTable(10)
+    small_table.set('John', 1234)
+    small_table.set('Jane', 123456)
+    small_table.set('John', '123')
+    small_table.set('Jane', 'abc')
+    return small_table
+
+
+@pytest.fixture
+def large_table():
+    """Large table."""
+    large_table = HashTable()
+    large_table.set('John', 1234)
+    large_table.set('Jane', 123456)
+    large_table.set('John', '123')
+    large_table.set('Jim', 'abc')
+    large_table.set('Jane', 123456)
+    large_table.set('Jane', '123')
+    large_table.set('Jane', 'abc')
+    return large_table
