6aaead3f6406b960f3905cac13d80426df19c770
Jay Adams <j.adams011@hotmail.com>
2018-05-07 18:34:27 -0700

    Finish code

--- sorting_algos/selection.py
+++ sorting_algos/selection.py
@@ -1,10 +1,24 @@
 """Selection Sort."""
 
 
-def selection_sort(arr):
+def selection_sort(lst):
     """Set up selection sort."""
-    for i in range(len(arr)-1, 0, -1):
-        index_max_val = 0
-        for j in range(1, i + 1):
-            if arr[location] > arr[index_max_val]:
-                index_max_val = j
+    if len(arr) < 2:
+        return lst
+
+    def smallest_index(arr):
+        """Define helper function."""
+        i = arr[0]
+        sm_index = 0
+        for i in range(len(arr)):
+            if arr[i] < i:
+                i = arr[i]
+                sm_index = j
+        return sm_index
+
+    result = []
+    for j in range(len(arr)):
+        i = smallest_index(arr)
+        lst.append(arr.pop(i))
+
+    return lst
